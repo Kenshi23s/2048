a62d99984e6980aa474322d7fc89ffd1d25e511f
Facundo Colombo <facucolombo@gmail.com>
2024-11-14 22:35:50 -0300

    Avances Input

--- 2048/GamePlay.py
+++ 2048/GamePlay.py
@@ -16,28 +16,35 @@ def FinDelJuego(tablero):
 # para testear lo jugariamos nosotros
 def JugarConInputs():
     tablero = GameLogic.CrearTablero()
+    print("Toca las flechas para jugar!")
+    print(tablero)
     while not FinDelJuego(tablero):
         # escuchar inputs de flechas
         # si es flecha izquierda no hacer nada
-        if input.is_pressed('right'):  # flecha arriba
+        if input.is_pressed('right'):  # flecha derecha
             tablero = Jugar(tablero)
 
         if input.is_pressed('up'):  # flecha arriba
             tablero = Jugar(np.rot90(tablero, 1))
+            tablero = np.rot90(tablero, -1)
 
-        if input.is_pressed('left'):  # flecha arriba
+        if input.is_pressed('left'):  # flecha izquierda
             tablero = Jugar(np.rot90(tablero, 2))
+            tablero = np.rot90(tablero, -2)
 
-        if input.is_pressed('down'):  # flecha arriba
+        if input.is_pressed('down'):  # flecha abajo
             tablero = Jugar(np.rot90(tablero, 3))
+            tablero = np.rot90(tablero, -3)
 
     print("se termino el juego!")
+    return tablero
 
 
 def Jugar(tablero):
     tablero = MoverHaciaIzquierdaTodo(tablero)
     tablero = LlenarCasilleroVacio(tablero)
     print(tablero)
+
     return tablero
 
 
--- 2048/Logic2048.py
+++ 2048/Logic2048.py
@@ -2,49 +2,49 @@ import numpy as np
 from random import randint as rd
 
 
-def LlenarCasilleroVacio(Tablero): #Busca todos los casilleros vacios, y elige uno al azar para poner el numero dos
+def LlenarCasilleroVacio(Tablero):  # Busca todos los casilleros vacios, y elige uno al azar para poner el numero dos
     CasillasVacias = []
     for i in range(Tablero.shape[0]):
         for j in range(Tablero.shape[1]):
             if Tablero[i][j] == 0:
-                CasillasVacias.append((i,j))
-    
+                CasillasVacias.append((i, j))
+
     Tablero[CasillasVacias[rd(0, len(CasillasVacias) - 1)]] = 2
+    return Tablero
 
 
-def CrearTablero(): #Crea una matriz de 4x4 con ints y llena dos posiciones azarosas diferentes con el valor 2
-    Tablero = np.zeros((4,4), int)
+def CrearTablero():  # Crea una matriz de 4x4 con ints y llena dos posiciones azarosas diferentes con el valor 2
+    Tablero = np.zeros((4, 4), int)
 
     LlenarCasilleroVacio(Tablero)
     LlenarCasilleroVacio(Tablero)
 
-    return Tablero 
+    return Tablero
+
 
 def MoverHaciaIzquierdaTodo(Tablero):
     for i in range(0, Tablero.shape[1]):
         for j in range(Tablero.shape[1]):
             MoverHaciaIzquierda(i, j, Tablero)
-    
+    return Tablero
 
 
 def MoverHaciaIzquierda(i, j, Tablero):
     while j >= 0:
-        if(j - 1) == -1: return
+        if (j - 1) == -1: return
 
-        if Tablero[(i, j - 1)] == Tablero[(i, j)]: #Fusionar
+        if Tablero[(i, j - 1)] == Tablero[(i, j)]:  # Fusionar
             Tablero[(i, j)] = 0
-            Tablero[(i, j - 1 )] *= Tablero[(i, j - 1 )]
+            Tablero[(i, j - 1)] *= Tablero[(i, j - 1)]
             return
-        
-        if Tablero[(i, j - 1)] != 0 and Tablero[(i, j - 1)] != Tablero[(i, j)]: #Colision entre fichas de diferente valor
+
+        if Tablero[(i, j - 1)] != 0 and Tablero[(i, j - 1)] != Tablero[
+            (i, j)]:  # Colision entre fichas de diferente valor
             return
-        
-        else: #Desplazar hacia la izquierda
-            Tablero[(i, j-1)] = Tablero[(i, j)]
+
+        else:  # Desplazar hacia la izquierda
+            Tablero[(i, j - 1)] = Tablero[(i, j)]
             Tablero[(i, j)] = 0
 
         j -= 1
-
-
-
-
+    return Tablero
