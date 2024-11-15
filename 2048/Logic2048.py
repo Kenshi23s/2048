30b84634facf541490d938496955756ae9c9e3c6
Tiziano Hrabinski Ruta <101399093+TizianoHrabinskiRuta@users.noreply.github.com>
2024-11-15 08:13:33 -0300

    Partially refactored to partially fit constraints

--- 2048/Logic2048.py
+++ 2048/Logic2048.py
@@ -2,24 +2,42 @@ import numpy as np
 from random import randint as rd
 
 
-def LlenarCasilleroVacio(Tablero): #Busca todos los casilleros vacios, y elige uno al azar para poner el numero dos
+def listar_pos_vacias(tablero):
     CasillasVacias = []
-    for i in range(Tablero.shape[0]):
-        for j in range(Tablero.shape[1]):
-            if Tablero[i][j] == 0:
+    for i in range(tablero.shape[0]):
+        for j in range(tablero.shape[1]):
+            if tablero[i][j] == 0:
                 CasillasVacias.append((i,j))
-    
+
+    return CasillasVacias
+
+#
+def llenar_pos_vacias(tablero, cantidad):
+    CasillasVacias = listar_pos_vacias(tablero)
+    if cantidad > len(CasillasVacias): cantidad = len(CasillasVacias)
+
+    for i in range(cantidad):
+        Indice = rd(0, len(CasillasVacias) - 1)
+        tablero[CasillasVacias[Indice]] = 2
+        CasillasVacias.pop(Indice)
+
+
+
+def LlenarCasilleroVacio(Tablero): #Busca todos los casilleros vacios, y elige uno al azar para poner el numero dos
+    CasillasVacias = listar_pos_vacias(Tablero)    
     Tablero[CasillasVacias[rd(0, len(CasillasVacias) - 1)]] = 2
+#
 
-
-def CrearTablero(): #Crea una matriz de 4x4 con ints y llena dos posiciones azarosas diferentes con el valor 2
+##
+def crear_tablero(n): #Crea una matriz de 4x4 con ints y llena dos posiciones azarosas diferentes con el valor 2
     Tablero = np.zeros((4,4), int)
 
-    LlenarCasilleroVacio(Tablero)
-    LlenarCasilleroVacio(Tablero)
+    llenar_pos_vacias(Tablero, 2)
 
     return Tablero 
+##
 
+###
 def MoverHaciaIzquierdaTodo(Tablero):
     for i in range(0, Tablero.shape[1]):
         for j in range(Tablero.shape[1]):
@@ -44,13 +62,15 @@ def MoverHaciaIzquierda(i, j, Tablero):
             Tablero[(i, j)] = 0
 
         j -= 1
+###
 
+####
 def RotarTableroHorizontalmenteNoventaGrados(Tablero):
     TableroNuevo = np.zeros((4,4), int)
     for i in range(4):
         for j in range(4):
             TableroNuevo[(j, abs(i - 3))] = Tablero[(i, j)]
-            
+
     return TableroNuevo
 
 def RotarTableroHorizontalmenteNoventaGradosVariasVeces(Tablero, Rotaciones):
@@ -63,34 +83,39 @@ def RotarTableroHorizontalmenteNoventaGradosVariasVeces(Tablero, Rotaciones):
         TableroNuevo = RotarTableroHorizontalmenteNoventaGrados(TableroNuevo)
 
     return TableroNuevo
+####
 
-def EfectuarMovimiento(Tablero, Movimiento):
-    if Movimiento == "Izquierda":
+#####
+def mover(Tablero, Movimiento):
+    if Movimiento == "izquierda":
         MoverHaciaIzquierdaTodo(Tablero)
     
-    elif Movimiento == "Derecha":
+    elif Movimiento == "derecha":
         Tablero = RotarTableroHorizontalmenteNoventaGradosVariasVeces(Tablero, 2)
         MoverHaciaIzquierdaTodo(Tablero)
         Tablero = RotarTableroHorizontalmenteNoventaGradosVariasVeces(Tablero, 2)
     
-    elif Movimiento == "Arriba":
+    elif Movimiento == "arriba":
         Tablero = RotarTableroHorizontalmenteNoventaGradosVariasVeces(Tablero, 3)
         MoverHaciaIzquierdaTodo(Tablero)
         Tablero = RotarTableroHorizontalmenteNoventaGradosVariasVeces(Tablero, 1)
     
-    elif Movimiento == "Abajo":
+    elif Movimiento == "abajo":
         Tablero = RotarTableroHorizontalmenteNoventaGradosVariasVeces(Tablero, 1)
         MoverHaciaIzquierdaTodo(Tablero)
         Tablero = RotarTableroHorizontalmenteNoventaGradosVariasVeces(Tablero, 3)
 
     LlenarCasilleroVacio(Tablero)
     return Tablero
+#####
 
 
-
-Tablero = CrearTablero()
+Tablero = crear_tablero(4)
+llenar_pos_vacias(Tablero, 2)
 print(Tablero, '\n')
-
-Tablero = EfectuarMovimiento(Tablero, "Arriba")
-
-print(Tablero)
+Tablero = mover(Tablero, "arriba")
+print(Tablero, '\n')
+Tablero = mover(Tablero, "derecha")
+print(Tablero, '\n')
+Tablero = mover(Tablero, "izquierda")
+print(Tablero, '\n')
---
+++ 2048/gui.py
@@ -0,0 +1,112 @@
+import pygame
+import sys
+import resolucion
+
+SCREENSIZE = 600
+
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)
+ZERO = (230, 230, 230)
+ONE = (255, 186, 8)
+TWO = (250, 163, 7)
+THREE = (244, 140, 6)
+FOUR = (232, 93, 4)
+FIVE = (220, 47, 2)
+SIX = (208, 0, 0)
+SEVEN = (157, 2, 8)
+EIGHT = (106, 4, 15)
+NINE = (55, 6, 23)
+TEN = (3, 7, 30)
+
+COLORS = {
+    0: ZERO,
+    2**1: ONE,
+    2**2: TWO,
+    2**3: THREE,
+    2**4: FOUR,
+    2**5: FIVE,
+    2**6: SIX,
+    2**7: SEVEN,
+    2**8: EIGHT,
+    2**9: NINE,
+    2**10: TEN
+}
+
+GAP = 10
+
+def display_message(screen, message, color):
+    screen.fill(WHITE)
+    font = pygame.font.Font(None, 50)
+    text = font.render(message, True, color)
+    text_rect = text.get_rect(center=(SCREENSIZE // 2, SCREENSIZE // 2))
+    screen.blit(text, text_rect)
+    pygame.display.flip()
+
+def display_arr(screen, arr, tile_size):
+    screen.fill(WHITE)
+    n, _ = arr.shape
+    tile_width = tile_size[0]
+    for i in range(n):
+        for j in range(n):
+            x = GAP + tile_width/2 + j * (tile_width + GAP)
+            y = GAP + tile_width/2 + i * (tile_width + GAP)
+            num = int(arr[(i, j)])
+            font = pygame.font.Font(None, int(tile_width/len(str(num))**0.3))
+            color = COLORS.get(num, BLACK)
+            rect = pygame.Rect(0, 0, *tile_size)
+            rect.center = (x, y)
+            bg_surface = pygame.Surface(tile_size)
+            bg_surface.fill(color)
+            screen.blit(bg_surface, rect)
+            if num != 0:
+                text = font.render(str(num), True, BLACK if num < 128 else WHITE)
+                offset = tile_width/2
+                text_rect = text.get_rect(center=(x, y + 0.05*tile_width))
+                screen.blit(text, text_rect)
+
+    pygame.display.flip()
+
+def main(n):
+    # Initialize Pygame
+    pygame.init()
+
+    # Set up display
+    screen = pygame.display.set_mode((SCREENSIZE, SCREENSIZE))
+
+    tablero = resolucion.crear_tablero(n)
+    tablero = resolucion.llenar_pos_vacias(tablero, 2)
+    tile_width = (SCREENSIZE - (n+1)*GAP)/n
+    tile_size = (tile_width, tile_width)
+
+    # Initial screen setup
+    screen.fill(WHITE)
+    pygame.display.flip()
+
+    # Main loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    tablero = resolucion.mover(tablero, "derecha")
+                elif event.key == pygame.K_LEFT:
+                    tablero = resolucion.mover(tablero, "izquierda")
+                elif event.key == pygame.K_UP:
+                    tablero = resolucion.mover(tablero, "arriba")
+                elif event.key == pygame.K_DOWN:
+                    tablero = resolucion.mover(tablero, "abajo")
+                elif event.key == pygame.K_RETURN and resolucion.esta_atascado(tablero):
+                    tablero = resolucion.crear_tablero(n)
+                    tablero = resolucion.llenar_pos_vacias(tablero, 2)
+
+        if resolucion.esta_atascado(tablero):
+            display_message(screen, "Presioná Enter para volver a jugar", RED)
+        else:
+            display_arr(screen, tablero, tile_size)
+
+main(4)
