1ffa017f19517c46bf5e8743fc929ec4e302c5e7
Facundo Colombo <facucolombo@gmail.com>
2024-11-16 00:03:03 -0300

    Update GamePlay.py

--- 2048/GamePlay.py
+++ 2048/GamePlay.py
@@ -1,3 +1,5 @@
+from tkinter.ttk import Frame
+
 import numpy as np
 import Logic2048 as GameLogic
 import pandas as pd
@@ -5,18 +7,19 @@ import random as rand
 
 movimientos = ["arriba", "derecha", "izquierda", "abajo"]
 
-#https://es.stackoverflow.com/questions/403245/c%C3%B3mo-unir-dataframes-en-pandas
+
+# https://es.stackoverflow.com/questions/403245/c%C3%B3mo-unir-dataframes-en-pandas
 def ProbarEstrategias(n):
     # generar n estrategias 
 
     masterFrame = ProbarEstrategia()
 
     for _ in range(n - 1):
-        masterFrame.ProbarEstrategia()
+        frameActual = ProbarEstrategia()
+        masterFrame = pd.concat([masterFrame, frameActual])
     # me gusta esto de _ cuando no usas el parametro, es como el descarte de c#
 
-    return
-
+    return masterFrame
 
 def ProbarEstrategia():
     estrategiaActual = GenerarEstrategia(rand.randint(1, 10))
@@ -27,33 +30,42 @@ def GenerarEstrategia(largo):
     max = len(movimientos) - 1
     estrategia = [""] * largo
     for i in range(largo):
-        estrategia[i] = rand.randint(0, max)
+        estrategia[i] = movimientos[rand.randint(0, max)]
     return estrategia
 
 
 def EjecutarEstrategia(listaDeComandos):  # lista de strings con comandos a usar en bucle
     df = pd.DataFrame()
     listaDeComandos = NormalizarMovimientos(listaDeComandos)
+
     tablero = GameLogic.crear_tablero(3)
     tablero = GameLogic.llenar_pos_vacias(tablero, 1)
-    i = 1000  # lo uso como watchdog para q no siga hasta el infinito
-    # historial = []
-    largo = len(listaDeComandos)
 
-    while not GameLogic.esta_atascado(tablero) and i >= 0:
+    i = 0
+    largo = len(listaDeComandos)
+    finJuego = False
+    while not finJuego and not GameLogic.esta_atascado(tablero) and i < 1000:  # es un watch dog
         comandoActual = listaDeComandos[i % largo]
         if not GameLogic.mover(tablero, comandoActual):
-            i = 0
-        i -= 1
+            finJuego = True
+        i += 1
         # historial.append(copy.deepcopy(tablero))
 
-    df["Estrategia"] = listaDeComandos
-    df["Cantidad de turnos"] = i
-    df["SumatoriaTotal"] = sum(tablero)
-    df["NumeroMasAlto"] = np.max(tablero)
-    df["TableroFinal"] = tablero
+    # Relleno Data frame
+    df["Estrategia"] = [PasarAString(listaDeComandos)]
+    df["Cantidad de turnos"] = [i]
+    df["SumatoriaTotal"] = [sum(tablero)]
+    df["NumeroMasAlto"] = [np.max(tablero)]
+    # df["TableroFinal"] = tablero nose si lo quiero
 
     return df
+
+
+def PasarAString(comandos):
+    palabra = ""
+    for x in comandos:
+        palabra += x[0:3] + ","
+    return palabra
 
 
 def NormalizarMovimientos(listaDeComandos):
