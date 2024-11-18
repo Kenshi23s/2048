06038bbd3641b10d398efbcfb90d643a8112f005
Facundo Colombo <facucolombo@gmail.com>
2024-11-17 21:27:59 -0300

    Avance

--- 2048/GamePlay.py
+++ 2048/GamePlay.py
@@ -1,5 +1,4 @@
-from tkinter.ttk import Frame
-
+import copy as cp
 import numpy as np
 import Logic2048 as GameLogic
 import pandas as pd
@@ -7,12 +6,54 @@ import random as rand
 
 movimientos = ["arriba", "derecha", "izquierda", "abajo"]
 
+stencil = np.array([
+    [4, 2, 2, 4],
+    [3, 1, 1, 3],
+    [3, 1, 1, 3],
+    [4, 2, 2, 4]
+])
+
 
 # https://es.stackoverflow.com/questions/403245/c%C3%B3mo-unir-dataframes-en-pandas
 
-#esto es muy "bongo sorting" quisiera ver como hacerlo en simultaneo en varios hilos
-#no esperaba q esto funcione, fue mas un experimento de hacerlo por la fuerza bruta
+# esto es muy "bongo sorting" quisiera ver como hacerlo en simultaneo en varios hilos
+# no esperaba q esto funcione, fue mas un experimento de hacerlo por la fuerza bruta
 # numero de bucles en 3 horas:6341031
+
+def GoapSimulacion():
+    tablero = GameLogic.crear_tablero(4)
+    tablero = GameLogic.llenar_pos_vacias(tablero, 2)
+
+    i = 0
+    finJuego = False
+    while not finJuego and not GameLogic.esta_atascado(tablero) and i < 1000   # es un watch dog
+
+        tablerosSimulados = ObtenerTablerosPosibles(tablero)
+        tablero = MejorTablero(tablerosSimulados)
+        tablero = GameLogic.llenar_pos_vacias(tablero, 1)
+        print(tablero)
+
+
+def MejorTablero(tableros):
+    tablerosDic = {}
+    for tablero in tableros:
+        key = np.sum(tablero * stencil)
+        tablerosDic = {key: tablero}
+
+    maximaKey = max(tablerosDic.keys())
+    return tablerosDic[maximaKey]
+
+
+def ObtenerTablerosPosibles(tablero):
+    tableros = []
+    for movimiento in movimientos:
+        tableroSimulado = cp.deepcopy(tablero)
+        if GameLogic.mover(tableroSimulado, movimiento):
+            tableros.append(tableroSimulado)
+    return tableros
+
+
+# --------------GOAP-----------
 def JugarHastaGanar():
     tablero = GameLogic.crear_tablero(3)
     tablero = GameLogic.llenar_pos_vacias(tablero, 1)
